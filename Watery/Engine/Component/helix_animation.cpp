ba1a32560cebf7bda5bc4adc4da33bceeceadb5f
mike <mango@live.cn>
2017-05-28 18:43:30 +0800

    Testing animation.

---
+++ Watery/Engine/Component/helix_animation.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Mike Smith on 2017/5/28.
+//
+
+#include "helix_animation.h"
---
+++ Watery/Engine/Component/helix_animation.h
@@ -0,0 +1,13 @@
+//
+// Created by Mike Smith on 2017/5/28.
+//
+
+#ifndef WATERY_HELIX_ANIMATION_H
+#define WATERY_HELIX_ANIMATION_H
+
+class helix_animation
+{
+
+};
+
+#endif  // WATERY_HELIX_ANIMATION_H
